8ee886961d4aa68c04de884488d32de4e3faf42d
sangwopa <79255978+sangwopa@users.noreply.github.com>
2022-07-27 20:36:40 +0900

    22.07.27

---
+++ else_func/model_split.py
@@ -0,0 +1,40 @@
+import os
+import pickle
+
+def model_split(source, dest_folder, write_size):
+    # Make a destination folder if it doesn't exist yet
+    if not os.path.exists(dest_folder):
+        os.mkdir(dest_folder)
+    else:
+        # Otherwise clean out all files in the destination folder
+        for file in os.listdir(dest_folder):
+            os.remove(os.path.join(dest_folder, file))
+    partnum = 0
+    
+    # Open the source file in binary mode
+    input_file = open(source, 'rb')
+    while True:
+        # Read a portion of the input file
+        chunk = input_file.read(write_size)
+        
+        # End the loop if we have hit EOF
+        if not chunk:
+            break
+        
+        # Increment partnum
+        partnum += 1
+        
+        # Create a new file name
+        filename = '/Model_Files/final_model' + str(partnum)
+        
+        # Create a destination file
+        dest_file = open(filename, 'wb')
+        
+        # Write to this portion of the destination file
+        dest_file.write(chunk)
+        # Explicitly close 
+        dest_file.close()
+    # Explicitly close
+    input_file.close()
+    # Return the number of files created by the split
+    return partnum
